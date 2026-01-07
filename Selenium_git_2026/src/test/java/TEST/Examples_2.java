5c3125a6b2d6510be1c86c2906b02a5a68cdae73
Gangabhavani <bavani_marri@yahoo.co.in>
2026-01-07 23:34:44 +0100

    second changes

---
+++ Selenium_git_2026/src/test/java/TEST/Examples_2.java
@@ -0,0 +1,23 @@
+package TEST;
+
+public class Examples_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Examples_2 e=new Examples_2();
+		e.method();
+		e.method1();
+
+	}
+	public void method() {
+		
+		System.out.println("i am from method");
+		
+	}
+public void method1() {
+		
+		System.out.println("i am from method1");
+		
+	}
+
+}
---
+++ Selenium_git_2026/src/test/java/TEST/Examples_3.java
@@ -0,0 +1,27 @@
+package TEST;
+
+public class Examples_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Examples_3 e=new Examples_3();
+		e.method();
+		e.method1();
+        e.method2();
+	}
+	public void method() {
+		
+		System.out.println("i am from method");
+		
+	}
+public void method1() {
+		
+		System.out.println("i am from method1");
+		
+	}
+public void method2() {
+	
+	System.out.println("i am from method2");
+	
+}
+}
